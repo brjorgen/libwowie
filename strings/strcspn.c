e58ad200f40742cfb96f7fa91ab0c90848e0cff8
Brian Jorgensen <bjorgensen@lyft.com>
2024-11-08 11:19:01 -0500

    literally just added an unused compiler attribute

--- strings/strcspn.c
+++ strings/strcspn.c
@@ -3,8 +3,8 @@
 
 size_t	strcspn(const char *s, const char *reject)
 {
-	const uint8_t	*s_ptr;
-	const uint8_t	*reject_ptr;
+	__attribute__((unused)) const uint8_t	*s_ptr;
+	__attribute__((unused)) const uint8_t	*reject_ptr;
 	size_t			seg_size;
 
 	s_ptr = (const uint8_t *)s;
