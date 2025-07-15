d0236b4f27a7e96cb73e7da414402f18134ffcb6
brjorgen <brjorgen98@gmail.com>
2025-07-14 23:57:26 -0400

    forgot to push this lol

--- data-structures/linked-lists/wowie_lists.h
+++ data-structures/linked-lists/wowie_lists.h
@@ -20,10 +20,10 @@ enum	lst_errors {
 	E_LST_FOLD_ATTEMPTED_NULL_HEAD,
 	E_LST_FOLD_TOO_SMALL,
 	E_LST_DEL_ATTEMPTED_NULL,
-	__E_LST_MAX_ERR,
 	E_ALLOC_ATTEMPTED_0_SIZE,
 	E_ALLOC_ATTEMPTED_NULL,
 	E_ALLOC_FAILED,
+	__E_LST_MAX_ERR,
 };
 
 typedef struct	s_list {
@@ -46,15 +46,16 @@ t_list		*lst_search_brute_no_ref(t_list *head, bool *(f (t_list *)));
 t_list		*lst_delete_link(t_list *head, t_list *to_delete, bool (f_data_delete)(t_list *));
 t_list		*lst_clear(t_list *head, void (f_data_delete)(t_list *));
 void		*lst_fold(t_list *head, t_list *(f)(t_list *, t_list *));
-t_list		*lst_map(t_list *head, void *(f)(t_list *));
 t_list		*lst_insert_link_next(t_list *current, t_list *new_link);
 t_list		*lst_insert_link_prev(t_list *head, t_list *current, t_list *new_link);
 t_list		*lst_find_prev(t_list *head, t_list *current);
 t_list		*lst_cpy_link(t_list *link);
 t_list		*lst_cpy_lst(t_list *head);
-t_list		*lst_mapr_inplace(t_list *head, void *(f)(t_list *));
+t_list		*lst_map(t_list *head, void *(f)(t_list *));
+t_list		*lst_map_cpy(t_list *head, void *(f)(t_list *));
+t_list		*lst_mapr(t_list *head, void *(f)(t_list *));
 t_list		*lst_mapr_cpy(t_list *head, void *(f)(t_list *));
 size_t		lst_len(t_list *head);
-t_list		*lst_delete_lst(t_list *head, bool (f_data_delete)(t_list *));
+void		lst_delete_lst(t_list **head, bool (f_data_delete)(t_list *));
 
 #endif /* _WOWIE_LISTS_H_ */
